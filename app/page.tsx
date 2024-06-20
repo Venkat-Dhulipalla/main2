03c87fb2c9c6c063d0aced428d12f43b8c28b6a2
Venkat Dhulipalla <venkatdhulipalla21@gmail.com>
2024-06-20 16:31:11 -0500

    first commist

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
-import Education from "@/components/education";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
